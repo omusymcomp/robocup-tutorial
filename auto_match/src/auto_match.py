b1343811779f513cc8d1135787b5510902a02596
藤川拓海 <halinezumi0711@gmail.com>
2024-07-09 13:46:36 +0900

    現在日時をメンバ変数で保持するように

--- auto_match/src/auto_match.py
+++ auto_match/src/auto_match.py
@@ -2,10 +2,12 @@ import subprocess
 import argparse
 import sys
 import os
+from datetime import datetime
 
 
 def main():
     parser = argparse.ArgumentParser()
+    parser.add_argument("-d", "--base_dir", dest="base_dir", default="$HOME/rcss", help="環境構築をするベースディレクトリを指定")
     args = parser.parse_args()
     setup_tools = AutoMatch(args)
 
@@ -13,7 +15,9 @@ def main():
 
 class AutoMatch:
     def __init__(self, args):
-        pass
+        now = datetime.now()
+        formatted_date_time = now.strftime("%Y_%m%d_%H%M_%S")
+        self.log_dir = args.base_dir + "/log_analysis/log/" + formatted_date_time
 
 
 
