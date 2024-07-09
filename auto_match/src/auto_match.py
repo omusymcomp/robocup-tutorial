b24d50a5404a37dbaad33c5747df4c57454319ae
藤川拓海 <halinezumi0711@gmail.com>
2024-07-09 13:39:32 +0900

    雛形の作成

---
+++ auto_match/src/auto_match.py
@@ -0,0 +1,22 @@
+import subprocess
+import argparse
+import sys
+import os
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    args = parser.parse_args()
+    setup_tools = AutoMatch(args)
+
+
+
+class AutoMatch:
+    def __init__(self, args):
+        pass
+
+
+
+
+if __name__ == "__main__":
+    main()
