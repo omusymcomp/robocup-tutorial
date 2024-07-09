d295add510835083cb2de1efd77cf6df74cb69b2
藤川拓海 <halinezumi0711@gmail.com>
2024-07-09 13:55:13 +0900

    左右両チームのバイナリまでのパスをメンバ変数で保持

--- auto_match/src/auto_match.py
+++ auto_match/src/auto_match.py
@@ -7,7 +7,9 @@ from datetime import datetime
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-d", "--base_dir", dest="base_dir", default="$HOME/rcss", help="環境構築をするベースディレクトリを指定")
+    parser.add_argument("-d", "--base_dir", dest="base_dir", default="$HOME/rcss", help="環境構築されているベースディレクトリを指定")
+    parser.add_argument("-l", "--left_team_name", dest="left_team_name", default="cyrus2022", help="左側のチーム名を指定")
+    parser.add_argument("-r", "--right_team_name", dest="right_team_name", default="yushan2022", help="右側のチーム名を指定")
     args = parser.parse_args()
     setup_tools = AutoMatch(args)
 
@@ -19,6 +21,8 @@ class AutoMatch:
         formatted_date_time = now.strftime("%Y_%m%d_%H%M_%S")
         self.log_dir = args.base_dir + "/log_analysis/log/" + formatted_date_time
         self.team_binary_dir = args.base_dir + "/teams"
+        self.left_team_dir = args.base_dir + "/" + args.left_team_name + "/start.sh"
+        self.right_team_dir = args.base_dir + "/" + args.right_team_name + "/start.sh"
 
 
 
