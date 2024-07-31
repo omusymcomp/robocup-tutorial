8dd4b7e77a57bad34e589efac366945bdaf9ea0b
okayama <okayama9393s@gmail.com>
2024-08-01 01:18:42 +0900

    HAMのインストールを削除

--- setup_teams.sh
+++ setup_teams.sh
@@ -4,12 +4,10 @@ sudo apt update -y
 sudo apt upgrade -y
 
 BASE_DIR="$HOME/rcss"
-HAM_DIR="${BASE_DIR}/HAM"
 HELIOS_DIR="${BASE_DIR}/HELIOS"
 HELIOS_BASE_DIR="${BASE_DIR}/teams/base_team"
 CONFIGURE_DIR="${BASE_DIR}/tools"
 
-mkdir -p ${HAM_DIR}
 mkdir -p ${HELIOS_DIR}
 mkdir -p ${HELIOS_BASE_DIR}
 
@@ -22,22 +20,6 @@ git checkout 4c45970
 ./configure --prefix=${HAM_DIR}
 make -j 4
 make install
-
-# HAMのコンパイル
-cd ${HAM_DIR}
-git clone git@github.com:omusymcomp/omuHam.git
-cd omuHam
-chmod 744 tensorflow_c++_install.sh
-# ubuntu22だとpythonコマンドがデフォルトでないので必須
-sudo apt install python-is-python3
-# 高負荷が長時間かかるため要注意
-./tensorflow_c++_install.sh
-./bootstrap
-./configure --with-librcsc=${HAM_DIR}
-make -j 4
-
-
-
 
 # librcscのコンパイル(HELIOS用)
 cd ${HELIOS_DIR}
@@ -58,8 +40,6 @@ git checkout develop
 ./configure --prefix=${HELIOS_DIR} --with-librcsc=${HELIOS_DIR}
 make -j 4
 make install
-
-
 
 
 # librcscのコンパイル(helios-base用)
